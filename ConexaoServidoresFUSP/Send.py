cbecf6e3f9edf3bda675f020a21f8963ef0cb6cd
Alan Pereira Cavalcante <134745847+Aln2k23@users.noreply.github.com>
2023-08-14 19:09:08 -0300

    Update Send.py

--- ConexaoServidoresFUSP/Send.py
+++ ConexaoServidoresFUSP/Send.py
@@ -2,11 +2,11 @@ import smtplib
 import email.message
 
 def send(email_destinatario, message):
-    password = 'flgvyttwsyrnmsbd'  # Senha App gmail gerada automática
+    password = '...'  # Senha App gmail gerada automática
 
 
     msg = email.message.Message()
-    msg['From'] = 'guimera.sistem@gmail.com'  # Remetente
+    msg['From'] = 'emailteste@gmail.com'  # Remetente
     msg['To'] = f'{email_destinatario}'  # destinatário
     msg['Subject'] = 'Houve um problema com o servidor' # Título
     descricao_email = f"""{message}"""
